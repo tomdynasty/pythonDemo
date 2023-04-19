effa9e802352271cb22bdaa56c917404ea1976f4
tom_chen <tom_chen@mail.chungyo.net>
2023-04-19 21:40:46 +0800

    [add] init the database file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from . import db
 
 
 def create_app(test_config=None):
@@ -11,6 +12,8 @@ def create_app(test_config=None):
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
+
+    db.init_app(app)
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
